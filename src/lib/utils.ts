189244813267bd4ac2c43a7737b744118bc36d50
Zaymoriel Vandrell <zaymorielvandrell@gmail.com>
2026-02-16 21:52:06 +0800

    feat: add shadcn-svelte UI components

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 import { dev } from "$app/environment";
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
 
 export const delay = () => {
   if (!dev) {
@@ -7,3 +13,10 @@ export const delay = () => {
 
   return new Promise((resolve) => setTimeout(resolve, 2000));
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChild<T> = T extends { child?: any } ? Omit<T, "child"> : T;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type WithoutChildren<T> = T extends { children?: any } ? Omit<T, "children"> : T;
+export type WithoutChildrenOrChild<T> = WithoutChildren<WithoutChild<T>>;
+export type WithElementRef<T, U extends HTMLElement = HTMLElement> = T & { ref?: U | null };
